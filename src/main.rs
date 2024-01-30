01e79df235161ac23ed144af14e16b8787112032
JShand18 <jor.shand@gmail.com>
2024-01-30 15:40:31 -0500

    Added caluclation of number of intersections

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,56 @@ use std::collections::HashMap;
 
 fn main() {
     println!("Hello, world!");
-    let rads = vec![0.73, 1.47, 1.77, 3.92];
-    let ids = vec![String::from("s1"), String::from("s2"), String::from("e1"), String::from("e2")];
+    let rads = vec![0.28, 0.73, 1.12, 1.47, 1.77, 3.92];
+    let ids = vec![String::from("s1"), String::from("e1"), String::from("s2"), String::from("e2"), String::from("s3"), String::from("e3")];
 
-//     intersecting_chords_tuple(&rads, &ids);
-    intersecting_chords_hashmap(&rads, &ids);
+    let chords: Vec<(&f32, &f32)> = connect_chords(&rads, &ids);
+    count_intersections(chords);
 }
 
-fn intersecting_chords_tuple(point_rads: &Vec<f32>, point_ids: &Vec<String>) -> i32{
 
-    // Mapping the identifiers and the radian measurement of the points into a tuple
-    let points: Vec<(&String, &f32)> = std::iter::zip(point_ids, point_rads).collect();
+fn count_intersections(chords: Vec<(&f32, &f32)>) -> i32 {
+    // Store for the final count of intersections
+    let mut count: i32 = 0;
 
-    for point in points{
-        println!("{}, {}", point.0, point.1)
+    // Two pointers to check intersecting chords
+    // Once the major chord has checked for intersection of minor chords
+    // minor chords do not need to go back and check major chord
+    for i in 0..chords.len()-1{
+        // Sanity check of loop logic
+        println!("----{}", &i);
+        println!("start:{}, end:{}", chords[i].0, chords[i].1);
+        for j in &i+1..chords.len(){
+            println!("\tstart:{}, end:{}", chords[j].0, chords[j].1);
+            let check: bool = is_intersection(chords[i], chords[j]);
+            if check{
+                count += 1;
+            }
+            // Sanity check of divsor logic
+            println!("{}", check);
+            println!("{}", count);
+        }
     }
-
-
-    0
+    count
 }
 
-fn intersecting_chords_hashmap(point_rads: &Vec<f32>, point_ids: &Vec<String>) -> i32{
+fn is_intersection(major: (&f32, &f32), minor: (&f32, &f32)) -> bool{
+    // Interpreting the major chord as dividing the circle in half
+    // Checking if the points of the minor chord both exists on the same half circle
+    if (major.0 < minor.0 && minor.0 < major.1) && (major.0 < minor.1 && minor.1 < major.1){
+        false
+    } else if (minor.0 < major.0 || minor.0 > major.1) && (minor.1 < major.0 || minor.1 > major.1){
+        false
+    } else{
+        true
+    }
+}
 
+
+// Mapping of radian measures and identifiers using a hashmap for quick retrieval and search
+fn connect_chords<'a>(point_rads: &'a Vec<f32>, point_ids: &'a Vec<String>) -> Vec<(&'a f32, &'a f32)>{
     // Mapping the identifiers and radian measures of the points into a hashmap
-    let points: HashMap<&String, &f32> = point_ids.into_iter().zip(point_rads.iter()).collect();
+    let points: HashMap<&String, &f32> = point_ids.into_iter().zip(point_rads.into_iter()).collect();
     // Initializing Vector to store tuples representing the chords
     let mut chords: Vec<(&f32, &f32)> = Vec::new();
 
@@ -33,24 +59,38 @@ fn intersecting_chords_hashmap(point_rads: &Vec<f32>, point_ids: &Vec<String>) -
     for (id, rad) in &points{
         // Only care about finding the start points for each chord
         if id[0..1] == String::from("s") {
-            // String concatenation to form key for endpoint
+            // String concatenation to form key for end point
             let mut end = String::from("e");
             end.push_str(&id[1..2]);
-
-            // HashMap quicker retrieval
-            chords.push((rad, points.get(&end).unwrap()));
-
-            // Sanity check to ensure that the points are matched properly
-            println!("{}, {}",&rad, points.get(&end).unwrap());
+            let end_rad: &f32 = points.get(&end).unwrap();
+            // Ordering the tuple so the lower radian measure is first
+            if rad > &end_rad{
+                chords.push((end_rad, rad));
+                println!("{}, {}",&end_rad, &rad);
+            } else{
+                chords.push((rad, end_rad));
+                println!("{}, {}",&rad, &end_rad);
+            }
         }
     }
-
-
-    0
+    // returning the vector of chords
+    chords
 }
 
 // #[test]
 // fn unit_test(){
 //     assert_eq!(intersecting_chords(), 0);
 // }
+
+// fn intersecting_chords_tuple(point_rads: &Vec<f32>, point_ids: &Vec<String>) -> i32{
+//
+//     // Mapping the identifiers and the radian measurement of the points into a tuple
+//     let points: Vec<(&String, &f32)> = std::iter::zip(point_ids, point_rads).collect();
+//
+//     for point in points{
+//         println!("{}, {}", point.0, point.1)
+//     }
+//     0
+// }
+
 
