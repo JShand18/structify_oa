17dfe9761a5fb18d036dd846841137f1c4485cfb
JShand18 <jor.shand@gmail.com>
2024-01-30 19:43:13 -0500

    Finish writing tests of intersection correctness and invalid lists

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 use std::collections::HashMap;
 
 fn main() {
-    let rads = vec![0.28, 0.73, 1.12, 1.47, 1.77, 3.92];
-    let ids = vec![String::from("s1"), String::from("e1"), String::from("s2"), String::from("e2"), String::from("s3"), String::from("e3")];
+    let rads = vec![0.28, 0.73, 1.12, 1.47, 1.84, 2.14, 2.77, 3.92];
+    let ids = vec![String::from("s1"), String::from("s2"), String::from("s3"), String::from("s4"), String::from("e1"),
+                    String::from("e2"), String::from("e3"), String::from("e4")];
 
     println!("{} Intersections", how_many_intersections((rads, ids)));
 }
 
 fn how_many_intersections(list_of_chords: (Vec<f32>, Vec<String>)) -> i32{
-    count_intersections(connect_chords(&list_of_chords.0, &list_of_chords.1))
+    if list_of_chords.0.len() != list_of_chords.1.len() { // Not accepting lists of different sizes
+        -1
+    } else if list_of_chords.0.len() == 0 || list_of_chords.1.len() == 0{ // Not accepting empty lists
+        -1
+    } else{
+        count_intersections(connect_chords(&list_of_chords.0, &list_of_chords.1))
+    }
 }
 
 fn count_intersections(chords: Vec<(&f32, &f32)>) -> i32 {
@@ -17,13 +24,13 @@ fn count_intersections(chords: Vec<(&f32, &f32)>) -> i32 {
     // Two pointers to check intersecting chords
     // Once the major chord has checked for intersection of minor chords
     // minor chords do not need to go back and check major chord
-    for i in 0..chords.len()-1{
+    for i in 0..chords.len(){
         for j in i+1..chords.len(){
             if is_intersection(chords[i], chords[j]){ count += 1; }
         }
     }
     // returning the count of intersections
-    return count;
+    count
 }
 
 // Identifying any intersections with the current "major" chord with the rest of the "minor" chords
@@ -56,22 +63,87 @@ fn connect_chords<'a>(point_rads: &'a Vec<f32>, point_ids: &'a Vec<String>) -> V
             // Ordering the tuple so the lower radian measure is first
             if rad > &end_rad{
                 chords.push((end_rad, rad));
-                println!("{}, {}",&end_rad, &rad);
             } else{
                 chords.push((rad, end_rad));
             }
         }
     }
     // returning the vector of chords
-    return chords;
+    chords
+}
+
+
+
+
+// TESTING SUITE
+#[test]
+fn single_chord(){
+    let rads = vec![0.9, 1.7];
+    let ids = vec![String::from("s1"),String::from("e1")];
+    assert_eq!(how_many_intersections((rads, ids)), 0);
 }
 
 #[test]
-fn unit_test(){
-    let rads = vec![0.28, 0.73, 1.12, 1.47, 1.77, 3.92];
-    let ids = vec![String::from("s1"), String::from("e1"), String::from("s2"), String::from("e2"), String::from("s3"), String::from("e3")];
+fn pair_non_intersecting(){
+    let rads = vec![0.9, 1.7, 1.3, 2.92];
+    let ids = vec![String::from("s1"), String::from("s2"), String::from("e1"), String::from("e2")];
     assert_eq!(how_many_intersections((rads, ids)), 0);
 }
+
+#[test]
+fn pair_intersecting(){
+    let rads = vec![0.78, 1.47, 1.77, 3.92];
+    let ids = vec![String::from("s1"), String::from("s2"), String::from("e1"), String::from("e2")];
+    assert_eq!(how_many_intersections((rads, ids)), 1);
+}
+
+#[test]
+fn multiple_non_intersecting(){
+    let rads = vec![0.28, 0.73, 1.12, 1.47, 1.84, 2.14, 2.77, 3.92];
+    let ids = vec![String::from("s1"), String::from("e1"), String::from("s2"), String::from("e2"), String::from("s3"),
+                    String::from("e3"), String::from("s4"), String::from("e4")];
+    assert_eq!(how_many_intersections((rads, ids)), 0);
+}
+
+#[test]
+fn multiple_intersecting(){
+    let rads = vec![0.28, 0.73, 1.12, 1.47, 1.84, 2.14, 2.77, 3.92];
+    let ids = vec![String::from("s1"), String::from("s2"), String::from("s3"), String::from("s4"), String::from("e1"),
+                    String::from("e2"), String::from("e3"), String::from("e4")];
+    assert_eq!(how_many_intersections((rads, ids)), 6);
+}
+
+#[test]
+fn unordered_identifiers(){
+    let rads = vec![1.04, 1.25, 1.57, 3.92, 4.18, 4.72, 4.77, 5.49, 5.65, 5.75];
+    let ids = vec![String::from("s1"), String::from("s2"), String::from("s3"), String::from("e4"), String::from("e1"),
+                    String::from("s4"), String::from("e3"), String::from("s5"), String::from("e2"), String::from("e5")];
+    assert_eq!(how_many_intersections((rads, ids)), 4);
+}
+
+#[test]
+fn empty_radian_measurements(){
+    let rads: Vec<f32> = Vec::new();
+    let ids = vec![String::from("s1"), String::from("s2"), String::from("e1"), String::from("e2")];
+    assert_eq!(how_many_intersections((rads, ids)), -1);
+}
+
+#[test]
+fn empty_identifiers(){
+    let rads = vec![0.9, 1.7, 1.3, 2.92];
+    let ids: Vec<String> = Vec::new();
+    assert_eq!(how_many_intersections((rads, ids)), -1);
+}
+
+#[test]
+fn mismatch_radians_and_identifiers(){
+    let rads = vec![0.28, 0.73, 1.12, 1.47, 1.84, 2.14];
+    let ids = vec![String::from("s1"), String::from("s2"), String::from("s3"), String::from("s4"), String::from("e1"),
+                    String::from("e2"), String::from("e3"), String::from("e4")];
+    assert_eq!(how_many_intersections((rads, ids)), -1);
+}
+
+
 
 
 
